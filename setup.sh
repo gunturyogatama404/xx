18af116b208757bf6d89ee95f34c27aae97f0ed1
gunturyogatama404 <99540625+gunturyogatama404@users.noreply.github.com>
2025-03-25 21:41:20 +0700

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+set -e  # Hentikan eksekusi jika terjadi error
+
+# Update paket dan install dependencies
+apt update && apt upgrade -y
+apt install -y ccze curl tcpdump net-tools docker.io xfce4 xfce4-goodies \
+               xorg dbus-x11 x11-xserver-utils xrdp ufw
+
+# Konfigurasi sesi XFCE4 untuk XRDP
+echo 'unset DBUS_SESSION_BUS_ADDRESS
+unset SESSION_MANAGER
+xfce4-session' > ~/.xsession
+chmod +x ~/.xsession
+
+# Install ulang XRDP (jika perlu)
+apt install --reinstall xrdp -y
+
+# Download dan install UpRock Mining
+wget -O uprock.deb https://app-download.uprock.com/UpRock-Mining-v0.0.8.deb
+apt install ./uprock.deb -y
+rm -f uprock.deb
+
+# Membersihkan paket yang tidak diperlukan
+apt autoremove -y
+
+# Konfigurasi firewall
+ufw allow 3389/tcp
+ufw enable
+
+# Pastikan direktori xrdp ada
+mkdir -p /etc/xrdp
+
+# Konfigurasi startwm.sh untuk XRDP
+echo '#!/bin/sh
+startxfce4' > /etc/xrdp/startwm.sh
+chmod +x /etc/xrdp/startwm.sh
+
+# Restart XRDP service
+systemctl restart xrdp
+
+echo "Setup selesai. Anda bisa terhubung ke XRDP di port 3389."
